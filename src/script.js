978ccf66d5d6ada9c4dd02c0a63ff253af5d709b
hari <haribp65@gmail.com>
2020-01-21 18:58:54 +0530

    v:2 TODO task with plain js

--- src/script.js
+++ src/script.js
@@ -1,19 +1,19 @@
 onload=()=>console.log("loaded ...")
-let pendingTasks=[];
+let pendingTasks;
 let addInput=document.getElementById('add-input');
 let taskDisplay=document.getElementById('task-display')
 
 function addTask(){
     
     let newTask=addInput.value;
-    pendingTasks.push(newTask);
+    pendingTasks=newTask;
     console.log("Added task value : "+newTask);
     localStorage.setItem("task",newTask);
     console.log("Pending tasks :"+pendingTasks);
     //taskDisplay.innerHTML=localStorage.getItem('task');
     //for(i=0;i<pendingTasks.length;i++){
        //taskDisplay.innerHTML=pendingTasks[i];
-       taskDisplay.appendChild(document.createTextNode(pendingTasks[i]))
+       taskDisplay.appendChild(document.createTextNode(pendingTasks))
        taskDisplay.appendChild(document.createElement("br"));
     //}
     
